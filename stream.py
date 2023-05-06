5744bd14387e906dbdc93b8a55e34b14f401e9ae
EdenItz <edenitz34@gmail.com>
2023-05-06 14:26:19 +0300

    50

---
+++ stream.py
@@ -0,0 +1,45 @@
+import threading
+
+class Stream:
+    def __init__(self):
+        self.items = []
+        self.operation = None
+        self.thread = threading.Thread(target=self.run)
+        self.thread.start()
+        
+    def add(self, item):
+        self.items.append(item)
+        
+    def forEach(self, func):
+        self.operation = lambda x: func(x)
+        
+    def apply(self, func):
+        self.operation = func
+        new_stream = Stream()
+        for item in self.items:
+            result = self.operation(item)
+            if result == True:
+                new_stream.add(item)
+            else:
+                new_stream.add(result)
+        return new_stream
+        
+    def run(self):
+        while True:
+            if self.operation and self.items:
+                item = self.items.pop(0)
+                result = self.operation(item)
+                if result == True:
+                    continue
+                elif result == False:
+                    break
+                else:
+                    self.add(result)
+            else:
+                continue
+        
+    def stop(self):
+        self.add(False)
+        self.thread.join()
+        self.operation = None
+        del self.items[:]
